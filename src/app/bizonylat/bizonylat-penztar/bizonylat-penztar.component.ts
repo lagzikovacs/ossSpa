a48bf6f23ec26ccf76092373f41754e8bf7b08eb
lagzikovacs <lks@docport.hu>
2018-09-21 12:40:17 +0200

    nah

--- src/app/bizonylat/bizonylat-penztar/bizonylat-penztar.component.ts
+++ src/app/bizonylat/bizonylat-penztar/bizonylat-penztar.component.ts
@@ -1,28 +1,74 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {BizonylatService} from "../bizonylat.service";
-import {PenztarService} from "../../penztar/penztar.service";
-import {ErrormodalComponent} from "../../errormodal/errormodal.component";
+import {Component, ViewChild} from '@angular/core';
+import {BizonylatService} from '../bizonylat.service';
+import {ErrormodalComponent} from '../../errormodal/errormodal.component';
+import {BizonylatEgyMode} from '../bizonylategymode';
+import {PenztartetelService} from '../../penztar/penztartetel/penztartetel.service';
+import {PenztartetelDto} from '../../penztar/penztartetel/penztarteteldto';
+import {BizonylatTipus} from '../bizonylattipus';
 
 @Component({
   selector: 'app-bizonylat-penztar',
   templateUrl: './bizonylat-penztar.component.html',
   styleUrls: ['./bizonylat-penztar.component.css']
 })
-export class BizonylatPenztarComponent implements OnInit {
+export class BizonylatPenztarComponent {
   @ViewChild(ErrormodalComponent) errormodal: ErrormodalComponent;
 
   bizonylatservice: BizonylatService;
   eppFrissit = false;
+  penztarindex = -1;
+  penztarkivalasztva = false;
+  megjegyzes = '';
 
-  constructor(private _penztarsevice: PenztarService,
+  constructor(private _penztartetelservice: PenztartetelService,
               bizonylatservice: BizonylatService) {
     this.bizonylatservice = bizonylatservice;
   }
 
-  ngOnInit() {
+  penztarvalasztas(i: number) {
+    this.penztarindex = i;
+    this.penztarkivalasztva = true;
   }
 
-  penztarvalasztas(i: number) {
+  onSubmit() {
+    let Dto: PenztartetelDto;
 
+    this.eppFrissit = true;
+    this._penztartetelservice.CreateNew()
+      .then(res => {
+        if (res.Error != null) {
+          throw res.Error;
+        }
+
+        Dto = res.Result[0];
+
+        Dto.PENZTARKOD = this.bizonylatservice.BizonylatPenztarDto[this.penztarindex].PENZTARKOD;
+        Dto.DATUM = this.bizonylatservice.Dto[this.bizonylatservice.DtoSelectedIndex].BIZONYLATKELTE;
+        Dto.JOGCIM = this.bizonylatservice.bizonylatTipus === BizonylatTipus.BejovoSzamla ? 'Bejövő számla' : 'Számla';
+        Dto.UGYFELNEV = this.bizonylatservice.Dto[this.bizonylatservice.DtoSelectedIndex].UGYFELNEV;
+        Dto.BIZONYLATSZAM = this.bizonylatservice.Dto[this.bizonylatservice.DtoSelectedIndex].BIZONYLATSZAM;
+        if (this.bizonylatservice.bizonylatTipus === BizonylatTipus.BejovoSzamla) {
+          Dto.KIADAS = this.bizonylatservice.Dto[this.bizonylatservice.DtoSelectedIndex].BRUTTO;
+        } else {
+          Dto.BEVETEL = this.bizonylatservice.Dto[this.bizonylatservice.DtoSelectedIndex].BRUTTO;
+        }
+        Dto.MEGJEGYZES = this.megjegyzes;
+
+        return this._penztartetelservice.Add(Dto);
+      })
+      .then(res1 => {
+        if (res1.Error != null) {
+          throw res1.Error;
+        }
+
+        this.bizonylatservice.EgyMode = BizonylatEgyMode.Blank;
+      })
+      .catch(err => {
+        this.eppFrissit = false;
+        this.errormodal.show(err);
+      });
+  }
+  cancel() {
+    this.bizonylatservice.EgyMode = BizonylatEgyMode.Blank;
   }
 }

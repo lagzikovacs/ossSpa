b2b2a7065eac98f49233eca9c844a530fa37370d
lagzikovacs <lks@docport.hu>
2018-11-14 23:27:22 +0100

    signalr basic

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,9 @@ import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Subscription} from 'rxjs/index';
 import {ErrormodalComponent} from '../errormodal/errormodal.component';
 import {LogonService} from '../logon/logon.service';
-import {SessionService} from "../session/session.service";
+import {SessionService} from '../session/session.service';
+
+declare var $;
 
 @Component({
   selector: 'app-dashboard',
@@ -18,14 +20,27 @@ export class DashboardComponent implements OnInit, OnDestroy {
   szerepkorkivalasztva: boolean;
   sessionservice: SessionService;
 
+  connection = $.hubConnection('https://docport.hu/ossrest/api/osshub', {useDefaultPath: false});
+  hubproxy = this.connection.createHubProxy('OssHub');
+
   constructor(private _logonservice: LogonService,
               sessionservice: SessionService) {
     this.sessionservice = sessionservice;
   }
 
   ngOnInit() {
+    this.connection.stateChanged(function (change) {
+      console.log(change);
+    });
+
     this._subscription = this._logonservice.SzerepkorKivalasztvaObservable().subscribe(uzenet => {
       this.szerepkorkivalasztva = (uzenet.szerepkorkivalasztva as boolean);
+
+      if (this.szerepkorkivalasztva) {
+        this.connection.start();
+      } else {
+        this.connection.stop();
+      }
     });
   }
 

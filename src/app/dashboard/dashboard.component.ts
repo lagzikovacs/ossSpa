5087f42a17e51b91efa6ad5a5289fb94c259b75f
lagzikovacs <lks@docport.hu>
2018-11-15 12:45:40 +0100

    signalr + beep javítva

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import {Subscription} from 'rxjs/index';
 import {ErrormodalComponent} from '../errormodal/errormodal.component';
 import {LogonService} from '../logon/logon.service';
 import {SessionService} from '../session/session.service';
+import {PlatformLocation} from '@angular/common';
 
 declare var $;
 
@@ -22,11 +23,17 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   connection = $.hubConnection('https://docport.hu/ossrest/api/osshub', {useDefaultPath: false});
   hubproxy = this.connection.createHubProxy('OssHub');
+  connected = false;
   utolsouzenet = '';
 
   constructor(private _logonservice: LogonService,
+              private _platformLocation: PlatformLocation,
               sessionservice: SessionService) {
     this.sessionservice = sessionservice;
+
+    console.log((_platformLocation as any).location);
+    console.log((_platformLocation as any).location.href);
+    console.log((_platformLocation as any).location.origin);
   }
 
   ngOnInit() {
@@ -42,35 +49,49 @@ export class DashboardComponent implements OnInit, OnDestroy {
       this.szerepkorkivalasztva = (uzenet.szerepkorkivalasztva as boolean);
 
       if (this.szerepkorkivalasztva) {
-        this.hubproxy.on('Uzenet', (message) => {
-          this.utolsouzenet = message;
-          this.beep();
-        });
-
-        this.connection.start()
-          .done((data: any) => {
-            this.utolsouzenet = 'OssHub OK';
-          })
-          .fail((error: any) => {
-            this.utolsouzenet = error;
-          });
+        this.startconnection();
       } else {
-        this.connection.stop();
-        this.hubproxy.off('Uzenet');
-        this.utolsouzenet = '';
+        this.stopconnection();
       }
     });
   }
 
+  startconnection() {
+    this.hubproxy.on('Uzenet', (message) => {
+      // TODO lehet listába tenni, stb.
+      this.utolsouzenet = message;
+      this.beep();
+    });
+
+    this.connection.start()
+      .done((data: any) => {
+        this.utolsouzenet = 'OssHub OK';
+        this.connected = true;
+      })
+      .fail((error: any) => {
+        this.utolsouzenet = error;
+      });
+  }
+  stopconnection() {
+    this.connection.stop();
+    this.hubproxy.off('Uzenet');
+    this.utolsouzenet = '';
+    this.connected = false;
+  }
+
   beep() {
     const audio = new Audio();
-    audio.src = '../assets/Door Bell.mp3';
+    audio.src = 'assets/doorbell.mp3';
     audio.load();
     audio.play();
   }
 
   ngOnDestroy() {
     this._subscription.unsubscribe();
+
+    if (this.connected) {
+      this.stopconnection();
+    }
 
     Object.keys(this).map(k => {
       (this[k]) = null;

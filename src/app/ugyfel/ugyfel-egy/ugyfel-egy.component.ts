0a94a45492ec891b4fbbaf5ef82121b15bf7ed41
lagzikovacs <lks@docport.hu>
2019-05-30 20:19:20 +0200

    vcard

--- src/app/ugyfel/ugyfel-egy/ugyfel-egy.component.ts
+++ src/app/ugyfel/ugyfel-egy/ugyfel-egy.component.ts
@@ -6,6 +6,7 @@ import {JogKod} from '../../enums/jogkod';
 import {UgyfelContainerMode} from '../ugyfelcontainermode';
 import {UgyfelEgyMode} from '../ugyfelegymode';
 import {rowanimation} from '../../animation/rowAnimation';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-ugyfel-egy',
@@ -53,6 +54,8 @@ export class UgyfelEgyComponent implements OnDestroy {
     this.ugyfelservice.EgyMode = UgyfelEgyMode.UgyfelterLink;
   }
   vcard() {
+    this.ugyfelservice.uj = false;
+    this.ugyfelservice.DtoEdited = Object.assign({}, this.ugyfelservice.Dto[this.ugyfelservice.DtoSelectedIndex]);
     this.ugyfelservice.EgyMode = UgyfelEgyMode.Vcard;
   }
 
@@ -110,6 +113,61 @@ export class UgyfelEgyComponent implements OnDestroy {
 
   CsoportCancel() {
     this.ugyfelservice.EgyMode = UgyfelEgyMode.Reszletek;
+  }
+
+  VcardLetoles() {
+    const encode = 'ENCODING=QUOTED-PRINTABLE;CHARSET=utf-8:';
+
+    const Ceg = this.ugyfelservice.DtoEdited.Ceg ? this.ugyfelservice.DtoEdited.Ceg : ';';
+    const Beosztas = this.ugyfelservice.DtoEdited.Beosztas ? this.ugyfelservice.DtoEdited.Beosztas : '';
+
+    const raw = 'BEGIN:VCARD\r\n' +
+      'VERSION:2.1\r\n' +
+      'FN;' + encode + this.ugyfelservice.DtoEdited.Nev + '\r\n' +
+      'N;' + encode + this.ugyfelservice.DtoEdited.Nev + ';;;\r\n' +
+      'ORG;' + encode + Ceg + '\r\n' +
+      'TITLE;' + encode + Beosztas + '\r\n' +
+      'ADR;WORK;PREF;' + encode + ';;' + this.ugyfelservice.DtoEdited.Kozterulet + ' ' +
+        this.ugyfelservice.DtoEdited.Kozterulettipus + ' ' + this.ugyfelservice.DtoEdited.Hazszam + ';' +
+        this.ugyfelservice.DtoEdited.Helysegnev + ';;' + this.ugyfelservice.DtoEdited.Iranyitoszam + '\r\n' +
+      'EMAIL:' + this.ugyfelservice.DtoEdited.Email + '\r\n' +
+      'TEL;TYPE=CELL:' + this.ugyfelservice.DtoEdited.Telefon + '\r\n' +
+      'END:VCARD';
+
+    const arrayUTF8 = this.toUTF8Array(raw);
+    const byteNumbers = new Uint8Array(arrayUTF8.length);
+    for (let i = 0; i < arrayUTF8.length; i++) {
+      byteNumbers[i] = arrayUTF8[i];
+    }
+
+    const blob = new Blob([byteNumbers], {type: 'text/html;charset=UTF-8;'});
+    FileSaver.saveAs(blob, this.ugyfelservice.DtoEdited.Nev + '.vcf');
+  }
+
+  toUTF8Array(str) {
+    const utf8 = [];
+    for (let i = 0; i < str.length; i++) {
+      let charcode = str.charCodeAt(i);
+      if (charcode < 0x80) {
+        utf8.push(charcode);
+      } else if (charcode < 0x800) {
+        utf8.push(0xc0 | (charcode >> 6),
+          0x80 | (charcode & 0x3f));
+      } else if (charcode < 0xd800 || charcode >= 0xe000) {
+        utf8.push(0xe0 | (charcode >> 12),
+          0x80 | ((charcode >> 6) & 0x3f),
+          0x80 | (charcode & 0x3f));
+      } else {
+        i++;
+        charcode = 0x10000 + (((charcode & 0x3ff) << 10)
+          | (str.charCodeAt(i) & 0x3ff));
+        utf8.push(0xf0 | (charcode >> 18),
+          0x80 | ((charcode >> 12) & 0x3f),
+          0x80 | ((charcode >> 6) & 0x3f),
+          0x80 | (charcode & 0x3f));
+      }
+    }
+    return utf8;
   }
 
   ngOnDestroy() {
--- src/app/ugyfel/ugyfel-vcard/ugyfel-vcard.component.ts
+++ src/app/ugyfel/ugyfel-vcard/ugyfel-vcard.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, Output} from '@angular/core';
+import {UgyfelDto} from '../ugyfeldto';
 
 @Component({
   selector: 'app-ugyfel-vcard',
   templateUrl: './ugyfel-vcard.component.html',
   styleUrls: ['./ugyfel-vcard.component.css']
 })
-export class UgyfelVcardComponent implements OnInit {
+export class UgyfelVcardComponent implements OnDestroy {
+  @Input() item: UgyfelDto;
+  @Input() eppFrissit: boolean;
+  @Output() LetoltesClick = new EventEmitter<void>();
 
-  constructor() { }
-
-  ngOnInit() {
+  letoltesClick() {
+    this.LetoltesClick.emit();
   }
 
+  ngOnDestroy() {
+    Object.keys(this).map(k => {
+      (this[k]) = null;
+    });
+  }
 }

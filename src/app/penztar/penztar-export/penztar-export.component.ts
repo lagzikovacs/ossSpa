c2f526d5f552c702d9f7f8dae988a25d67b670fe
lagzikovacs <lks@docport.hu>
2019-07-29 13:16:05 +0200

    pénztártételek is az egységes riport módon

--- src/app/penztar/penztar-export/penztar-export.component.ts
+++ src/app/penztar/penztar-export/penztar-export.component.ts
@@ -1,48 +1,38 @@
 import {Component, Input, OnDestroy} from '@angular/core';
 import * as moment from 'moment';
-import * as FileSaver from 'file-saver';
-import {b64toBlob} from '../../tools/b64toBlob';
 import {SzMT} from '../../dtos/szmt';
 import {Szempont} from '../../enums/szempont';
 import {RiportService} from '../../riport/riport.service';
 import {ErrorService} from '../../tools/errorbox/error.service';
 import {SpinnerService} from '../../tools/spinner/spinner.service';
+import {Riportciklus} from '../../riport/riportciklus';
 
 @Component({
   selector: 'app-penztar-export',
   templateUrl: './penztar-export.component.html'
 })
 export class PenztarExportComponent implements OnDestroy {
+  rc: Riportciklus;
   @Input() Penztarkod = -1;
-
-  megszakitani = false;
 
   tol = '2018-01-01';
   ig = '2018-12-31';
 
-  tasktoken = '';
-  szamlalo: any;
-
-  private _eppFrissit = false;
-  get eppFrissit(): boolean {
-    return this._eppFrissit;
-  }
-  set eppFrissit(value: boolean) {
-    this._eppFrissit = value;
-    this._spinnerservice.Run = value;
-  }
-
   riportservice: RiportService;
+  spinnerservice: SpinnerService;
 
   constructor(private _errorservice: ErrorService,
-              private _spinnerservice: SpinnerService,
+              spinnerservice: SpinnerService,
               riportservice: RiportService) {
     this.riportservice = riportservice;
+    this.spinnerservice = spinnerservice;
+
+    this.rc = new Riportciklus(_errorservice, spinnerservice, riportservice, 'Pénztártétel.xls');
   }
 
   onSubmit() {
-    this.eppFrissit = true;
-    this.megszakitani = false;
+    this.spinnerservice.eppFrissit = true;
+    this.rc.megszakitani = false;
 
     const fi = [
       new SzMT(Szempont.Null, this.Penztarkod.toString()),
@@ -56,70 +46,20 @@ export class PenztarExportComponent implements OnDestroy {
           throw res.Error;
         }
 
-        this.tasktoken = res.Result;
-        this.ciklus();
+        this.rc.tasktoken = res.Result;
+        this.rc.ciklus();
       })
       .catch(err => {
-        this.eppFrissit = false;
+        this.spinnerservice.eppFrissit = false;
         this._errorservice.Error = err;
       });
-  }
-
-  ciklus() {
-    this.riportservice.TaskCheck(this.tasktoken)
-      .then(res => {
-        if (res.Error != null) {
-          throw res.Error;
-        }
-        if (res.Status === 'Cancelled') {
-          throw new Error('Felhasználói megszakítás!');
-        }
-        if (res.Status === 'Error') {
-          throw new Error('Hm... ' + res.Error);
-        }
-        if (res.Status === 'Queued' || res.Status === 'Running') {
-          this.szamlalo = setInterval(() => { this.next(); }, 1000);
-        }
-
-        if (res.Status === 'Completed') {
-          const blob = b64toBlob(res.Riport);
-          FileSaver.saveAs(blob, 'Pénztártételek.xls');
-          this.eppFrissit = false;
-        }
-      })
-      .catch(err => {
-        this.eppFrissit = false;
-        this._errorservice.Error = err;
-      });
-  }
-  next() {
-    clearInterval(this.szamlalo);
-
-    if (this.megszakitani) {
-      this.riportservice.TaskCancel(this.tasktoken)
-        .then(res => {
-          if (res.Error != null) {
-            throw res.Error;
-          }
-
-          this.eppFrissit = false;
-        })
-        .catch(err => {
-          this.eppFrissit = false;
-          this._errorservice.Error = err;
-        });
-    } else {
-      this.ciklus();
-    }
   }
 
   onCancel() {
-    this.megszakitani = true;
+    this.rc.megszakitani = true;
   }
 
   ngOnDestroy() {
-    clearInterval(this.szamlalo);
-
     Object.keys(this).map(k => {
       (this[k]) = null;
     });

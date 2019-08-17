0e075be33de070c084244d59f11cb31ec5272444
lagzikovacs <lks@docport.hu>
2019-08-17 07:22:12 +0200

    JSON konfig a partícióból

--- src/app/particio/particiodto.ts
+++ src/app/particio/particiodto.ts
@@ -2,49 +2,6 @@ export class ParticioDto {
   Particiokod: number;
   Megnevezes: string;
 
-  SzallitoNev: string;
-  SzallitoIranyitoszam: string;
-  SzallitoHelysegnev: string;
-  SzallitoUtcahazszam: string;
-  SzallitoBankszamla1: string;
-  SzallitoBankszamla2: string;
-  SzallitoAdotorzsszam: string;
-  SzallitoAdoafakod: string;
-  SzallitoAdomegyekod: string;
-
-  NavUrl: string;
-  NavFelhasznaloazonosito: string;
-  NavFelhasznalojelszo: string;
-  NavAlairokulcs: string;
-  NavCserekulcs: string;
-
-  SmtpKlienstipus: string;
-  SmtpFelhasznaloazonosito: string;
-  SmtpFelhasznalojelszo: string;
-  SmtpKuldoneve: string;
-  SmtpKuldoemailcime: string;
-  SmtpCustomhost: string;
-  SmtpCustomport: number;
-  SmtpTls: boolean;
-  Hibaertesitesemailcimek: string;
-
-  BizonylatBizonylatkepIratkod: number;
-  BizonylatEredetipeldanyokSzama: number;
-  BizonylatMasolatokSzama: number;
-
-  ProjektAjanlatIratkod: number;
-  ProjektElegedettsegifelmeresIratkod: number;
-  ProjektKeszrejelentesDemaszIratkod: number;
-  ProjektKeszrejelentesElmuemaszIratkod: number;
-  ProjektKeszrejelentesEonIratkod: number;
-  ProjektMunkalapIratkod: number;
-  ProjektSzallitasiszerzodesIratkod: number;
-  ProjektSzerzodesIratkod: number;
-  ProjektFeltetelesszerzodesIratkod: number;
-
-  VolumeUjvolumeMaxmeret: number;
-  VolumeUjvolumeEleresiut: string;
-
   Szallito: string;
   Navonlineszamla: string;
   Bizonylat: string;

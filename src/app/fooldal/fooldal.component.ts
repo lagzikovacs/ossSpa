e3a63a729749f7fcda8f6a41175a20f36748953c
lks <lks@gridsolar.hu>
2021-11-16 23:02:13 +0100

    bootstrap kifelé

--- src/app/fooldal/fooldal.component.ts
+++ src/app/fooldal/fooldal.component.ts
@@ -1,37 +1,8 @@
-import {Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-fooldal',
   templateUrl: './fooldal.component.html'
 })
-export class FooldalComponent implements OnInit, OnDestroy {
-  @ViewChild('hatterkep', {static: true}) hatterkep: ElementRef;
-
-  ngOnInit() {
-    this.resize();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    event.stopPropagation();
-    event.preventDefault();
-
-    this.resize();
-  }
-
-  resize() {
-    if (window.innerWidth > window.innerHeight) {
-      this.hatterkep.nativeElement.width = window.innerWidth;
-      this.hatterkep.nativeElement.height = window.innerHeight;
-    } else {
-      this.hatterkep.nativeElement.width = 0;
-      this.hatterkep.nativeElement.height = 0;
-    }
-  }
-
-  ngOnDestroy(): void {
-    Object.keys(this).map(k => {
-      (this[k]) = null;
-    });
-  }
+export class FooldalComponent {
 }

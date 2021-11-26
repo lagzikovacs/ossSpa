2e5444d0e9d1ec15b2b23dc240905f7d6f62fba4
lks <lks@gridsolar.hu>
2021-11-26 18:02:52 +0100

    errorbox

--- src/app/tools/errorbox/errorbox.component.ts
+++ src/app/tools/errorbox/errorbox.component.ts
@@ -1,33 +1,38 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 import {ErrorService} from './error.service';
-import {Subscription} from 'rxjs';
+import {OnDestroyMixin, untilComponentDestroyed} from '@w11k/ngx-componentdestroyed';
 
 @Component({
   selector: 'app-errorbox',
   templateUrl: './errorbox.component.html',
   styleUrls: ['./errorbox.component.css']
 })
-export class ErrorboxComponent implements OnInit, OnDestroy {
-  private subscription: Subscription;
-
+export class ErrorboxComponent extends OnDestroyMixin implements OnInit, OnDestroy {
+  errorbox: any;
   hiba: any;
   ahibastring = true;
 
   constructor(private _router: Router,
-              private _errorservice: ErrorService) { }
+              private _errorservice: ErrorService) {
+    super();
+  }
 
   ngOnInit() {
-    this.subscription = this._errorservice.ErrormessageObservable().subscribe(() => {
+    this.errorbox = document.getElementById('errorbox');
+
+    this._errorservice.ErrormessageObservable().pipe(untilComponentDestroyed(this)).subscribe(() => {
       this.hiba = this._errorservice.hiba;
 
       this.ahibastring = typeof(this.hiba) === 'string';
 
-      $('#errorbox').modal(); // a modal bootstrap extension a jqueryhez
+      this.errorbox.style.display = 'block';
     });
   }
 
   onClose() {
+    this.errorbox.style.display = 'none';
+
     if (this.ahibastring) {
       if (this.hiba.indexOf('Ismeretlen Sid') !== -1) {
         this._router.navigate(['/bejelentkezes']);
@@ -36,7 +41,7 @@ export class ErrorboxComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    super.ngOnDestroy();
 
     Object.keys(this).map(k => {
       (this[k]) = null;

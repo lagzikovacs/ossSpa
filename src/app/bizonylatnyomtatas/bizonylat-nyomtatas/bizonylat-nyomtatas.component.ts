51828fddee3a314ce1aa9923c4cadc5b1384986c
lks <lks@gridsolar.hu>
2021-12-07 21:59:53 +0100

    reaktiv este

--- src/app/bizonylatnyomtatas/bizonylat-nyomtatas/bizonylat-nyomtatas.component.ts
+++ src/app/bizonylatnyomtatas/bizonylat-nyomtatas/bizonylat-nyomtatas.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnDestroy} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {BizonylatnyomtatasService} from '../bizonylatnyomtatas.service';
 import {SzMT} from '../../dtos/szmt';
 import {Szempont} from '../../enums/szempont';
@@ -6,12 +6,13 @@ import {BizonylatNyomtatasTipus} from '../bizonylatnyomtatastipus';
 import {ErrorService} from '../../tools/errorbox/error.service';
 import {Bizonylatnyomtatasciklus} from '../bizonylatnyomtatasciklus';
 import {BizonylatService} from '../../bizonylat/bizonylat.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-bizonylat-nyomtatas',
   templateUrl: './bizonylat-nyomtatas.component.html'
 })
-export class BizonylatNyomtatasComponent implements OnDestroy {
+export class BizonylatNyomtatasComponent implements OnInit, OnDestroy {
   bc: Bizonylatnyomtatasciklus;
 
   @Input() Bizonylatkod = -1;
@@ -19,11 +20,18 @@ export class BizonylatNyomtatasComponent implements OnDestroy {
   entries = ['Minta', 'Eredeti', 'Másolat'];
   entriest = [BizonylatNyomtatasTipus.Minta, BizonylatNyomtatasTipus.Eredeti, BizonylatNyomtatasTipus.Masolat];
   selectedi = 0;
+
+  form: FormGroup;
   eppFrissit = false;
 
   constructor(private _bizonylatservice: BizonylatService,
+              private _fb: FormBuilder,
               private _bizonylatnyomtatasservice: BizonylatnyomtatasService,
               private _errorservice: ErrorService) {
+
+    this.form = this._fb.group({
+      'nyomtatastipus': [0, [Validators.required]]
+    });
 
     this.bc = new Bizonylatnyomtatasciklus(_errorservice, _bizonylatnyomtatasservice);
     this.bc.eventSpinnervege.on(() => {
@@ -31,20 +39,28 @@ export class BizonylatNyomtatasComponent implements OnDestroy {
     });
   }
 
-  change(i) {
-    this.selectedi = i;
+  ngOnInit() {
+    this.updateform();
+  }
+
+  updateform() {
+    this.form.controls['nyomtatastipus'].setValue(this.selectedi);
+  }
+  updatedto() {
+    this.selectedi = this.form.value['nyomtatastipus'];
   }
 
   onSubmit() {
     this.eppFrissit = true;
     this.bc.megszakitani = false;
     this.bc.fajlnev = '';
+    this.updatedto();
 
     const fi = [
       new SzMT(Szempont.BizonylatKod, this.Bizonylatkod),
       new SzMT(Szempont.NyomtatasTipus, this.entriest[this.selectedi])
     ];
-
+    console.log(fi);
     this._bizonylatservice.Get(this.Bizonylatkod)
       .then(res => {
         if (res.Error != null) {

e4e6602bcd85b00f4c1b849794d14ac0d3534c39
lks <lks@gridsolar.hu>
2021-12-11 18:53:58 +0100

    ügyfel reaktiv

--- src/app/ugyfel/ugyfel-szerkesztes/ugyfel-szerkesztes.component.ts
+++ src/app/ugyfel/ugyfel-szerkesztes/ugyfel-szerkesztes.component.ts
@@ -10,6 +10,7 @@ import {UgyfelDto} from '../ugyfeldto';
 import {TevekenysegDto} from '../../primitiv/tevekenyseg/tevekenysegdto';
 import {TevekenysegService} from '../../primitiv/tevekenyseg/tevekenyseg.service';
 import {TevekenysegZoomParameter} from '../../primitiv/tevekenyseg/tevekenysegzoomparameter';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-ugyfel-szerkesztes',
@@ -23,6 +24,7 @@ export class UgyfelSzerkesztesComponent implements OnInit, OnDestroy {
   }
   @Output() eventSzerkeszteskesz = new EventEmitter<UgyfelDto>();
 
+  form: FormGroup;
   eppFrissit = false;
 
   ugyfelzoombox: any;
@@ -34,8 +36,28 @@ export class UgyfelSzerkesztesComponent implements OnInit, OnDestroy {
   constructor(private _helysegservice: HelysegService,
               private _tevekenysegservice: TevekenysegService,
               private _errorservice: ErrorService,
+              private _fb: FormBuilder,
               ugyfelservice: UgyfelService) {
     this.ugyfelservice = ugyfelservice;
+
+    this.form = this._fb.group({
+      'nev': ['', [Validators.required, Validators.maxLength(200)]],
+      'iranyitoszam': ['', [Validators.required, Validators.maxLength(20)]],
+      'helysegnev': ['', [Validators.required, Validators.maxLength(100)]],
+      'kozterulet': ['', [Validators.required, Validators.maxLength(100)]],
+      'kozterulettipus': ['', [Validators.required, Validators.maxLength(20)]],
+      'hazszam': ['', [Validators.required, Validators.maxLength(10)]],
+      'telefon': ['', [Validators.required, Validators.maxLength(20)]],
+      'email': ['', [Validators.required, Validators.maxLength(50)]],
+      'adoszam': ['', [Validators.maxLength(20)]],
+      'euadoszam': ['', [Validators.maxLength(20)]],
+      'ceg': ['', [Validators.maxLength(200)]],
+      'beosztas': ['', [Validators.maxLength(200)]],
+      'tevekenyseg': ['', [Validators.required, Validators.maxLength(100)]],
+      'egyeblink': ['', [Validators.maxLength(200)]],
+      'ajanlotta': ['', [Validators.maxLength(200)]],
+      'megjegyzes': ['', [Validators.maxLength(200)]],
+    });
   }
 
   ngOnInit() {
@@ -50,17 +72,59 @@ export class UgyfelSzerkesztesComponent implements OnInit, OnDestroy {
           }
 
           this.DtoEdited = res.Result[0];
+          this.updateform();
           this.eppFrissit = false;
         })
         .catch(err => {
           this.eppFrissit = false;
           this._errorservice.Error = err;
         });
+    } else {
+      this.updateform();
     }
+  }
+
+  updateform() {
+    this.form.controls['nev'].setValue(this.DtoEdited.Nev);
+    this.form.controls['iranyitoszam'].setValue(this.DtoEdited.Iranyitoszam);
+    this.form.controls['helysegnev'].setValue(this.DtoEdited.Helysegnev);
+    this.form.controls['kozterulet'].setValue(this.DtoEdited.Kozterulet);
+    this.form.controls['kozterulettipus'].setValue(this.DtoEdited.Kozterulettipus);
+    this.form.controls['hazszam'].setValue(this.DtoEdited.Hazszam);
+    this.form.controls['telefon'].setValue(this.DtoEdited.Telefon);
+    this.form.controls['email'].setValue(this.DtoEdited.Email);
+    this.form.controls['adoszam'].setValue(this.DtoEdited.Adoszam);
+    this.form.controls['euadoszam'].setValue(this.DtoEdited.Euadoszam);
+    this.form.controls['ceg'].setValue(this.DtoEdited.Ceg);
+    this.form.controls['beosztas'].setValue(this.DtoEdited.Beosztas);
+    this.form.controls['tevekenyseg'].setValue(this.DtoEdited.Tevekenyseg);
+    this.form.controls['egyeblink'].setValue(this.DtoEdited.Egyeblink);
+    this.form.controls['ajanlotta'].setValue(this.DtoEdited.Ajanlotta);
+    this.form.controls['megjegyzes'].setValue(this.DtoEdited.Megjegyzes);
+  }
+  updatedto() {
+    this.DtoEdited.Nev = this.form.value['nev'];
+    this.DtoEdited.Iranyitoszam = this.form.value['iranyitoszam'];
+    this.DtoEdited.Helysegnev = this.form.value['helysegnev'];
+    this.DtoEdited.Kozterulet = this.form.value['kozterulet'];
+    this.DtoEdited.Kozterulettipus = this.form.value['kozterulettipus'];
+    this.DtoEdited.Hazszam = this.form.value['hazszam'];
+    this.DtoEdited.Telefon = this.form.value['telefon'];
+    this.DtoEdited.Email = this.form.value['email'];
+    this.DtoEdited.Adoszam = this.form.value['adoszam'];
+    this.DtoEdited.Euadoszam = this.form.value['euadoszam'];
+    this.DtoEdited.Ceg = this.form.value['ceg'];
+    this.DtoEdited.Beosztas = this.form.value['beosztas'];
+    this.DtoEdited.Tevekenyseg = this.form.value['tevekenyseg'];
+    this.DtoEdited.Egyeblink = this.form.value['egyeblink'];
+    this.DtoEdited.Ajanlotta = this.form.value['ajanlotta'];
+    this.DtoEdited.Megjegyzes = this.form.value['megjegyzes'];
   }
 
   onSubmit() {
     this.eppFrissit = true;
+    this.updatedto();
+
     this._helysegservice.ZoomCheck(new HelysegZoomParameter(this.DtoEdited.Helysegkod || 0,
       this.DtoEdited.Helysegnev || ''))
       .then(res => {
@@ -108,12 +172,14 @@ export class UgyfelSzerkesztesComponent implements OnInit, OnDestroy {
   }
 
   HelysegZoom() {
+    this.updatedto();
     this.SzerkesztesMode = UgyfelSzerkesztesMode.HelysegZoom;
     this.ugyfelzoombox.style.display = 'block';
   }
   onHelysegSelectzoom(Dto: HelysegDto) {
     this.DtoEdited.Helysegkod = Dto.Helysegkod;
     this.DtoEdited.Helysegnev = Dto.Helysegnev;
+    this.updateform();
     this.ugyfelzoombox.style.display = 'none';
   }
   onHelysegStopzoom() {
@@ -122,12 +188,14 @@ export class UgyfelSzerkesztesComponent implements OnInit, OnDestroy {
   }
 
   TevekenysegZoom() {
+    this.updatedto();
     this.SzerkesztesMode = UgyfelSzerkesztesMode.TevekenysegZoom;
     this.ugyfelzoombox.style.display = 'block';
   }
   onTevekenysegSelectzoom(Dto: TevekenysegDto) {
     this.DtoEdited.Tevekenysegkod = Dto.Tevekenysegkod;
     this.DtoEdited.Tevekenyseg = Dto.Tevekenyseg1;
+    this.updateform();
     this.ugyfelzoombox.style.display = 'none';
   }
   onTevekenysegStopzoom() {

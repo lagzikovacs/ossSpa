a33100e3a4befbcc82cba0156a361b8df18a8ba2
lks <lks@gridsolar.hu>
2021-12-12 20:48:34 +0100

    ngm

--- src/app/ngm/ngm.component.ts
+++ src/app/ngm/ngm.component.ts
@@ -1,49 +1,58 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgmService} from './ngm.service';
 import {NGMParam} from './ngmparam';
 import * as moment from 'moment';
 import {NGMMode} from './ngmmode';
 import {ErrorService} from '../tools/errorbox/error.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-ngm',
   templateUrl: './ngm.component.html'
 })
-export class NgmComponent implements OnDestroy {
-  ngmservice: NgmService;
-
-  szktol = '2018-01-01';
-  szkig = '2018-12-31';
-  szsztol = '';
-  szszig = '';
+export class NgmComponent implements OnInit, OnDestroy {
+  vszktol = '2021-01-01';
+  vszkig = '2021-12-31';
 
   np = new NGMParam();
   result = '';
 
+  form: FormGroup;
   eppFrissit = false;
 
+  ngmservice: NgmService;
+
   constructor(ngmservice: NgmService,
+              private _fb: FormBuilder,
               private _errorservice: ErrorService) {
     this.ngmservice = ngmservice;
+
+    this.form = this._fb.group({
+      'szktol': ['', []],
+      'szkig': ['', []],
+      'szsztol': ['', [Validators.maxLength(20)]],
+      'szszig': ['', [Validators.maxLength(20)]]
+    });
   }
 
-  submit() {
-
+  ngOnInit() {
+    this.form.controls['szktol'].setValue(this.vszktol);
+    this.form.controls['szkig'].setValue(this.vszkig);
   }
 
   szamlakelte() {
     this.np = new NGMParam();
     this.np.Mode = NGMMode.SzamlaKelte;
-    this.np.SzamlaKelteTol = moment(this.szktol).toISOString(true);
-    this.np.SzamlaKelteIg = moment(this.szkig).toISOString(true);
+    this.np.SzamlaKelteTol = moment(this.form.value['szktol']).toISOString(true);
+    this.np.SzamlaKelteIg = moment(this.form.value['szkig']).toISOString(true);
 
     this.adatszolgaltatas();
   }
   szamlaszam() {
     this.np = new NGMParam();
     this.np.Mode = NGMMode.SzamlaSzam;
-    this.np.SzamlaSzamTol = this.szsztol;
-    this.np.SzamlaSzamIg = this.szszig;
+    this.np.SzamlaSzamTol = this.form.value['szsztol'];
+    this.np.SzamlaSzamIg = this.form.value['szszig'];
 
     this.adatszolgaltatas();
   }

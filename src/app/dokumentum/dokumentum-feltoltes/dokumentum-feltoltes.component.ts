65118d31d3056f38e7498d70e0412a72482e3b65
lks <lks@gridsolar.hu>
2021-12-13 20:12:45 +0100

    este

--- src/app/dokumentum/dokumentum-feltoltes/dokumentum-feltoltes.component.ts
+++ src/app/dokumentum/dokumentum-feltoltes/dokumentum-feltoltes.component.ts
@@ -1,57 +1,61 @@
-import {Component, ElementRef, EventEmitter, Input, OnDestroy, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, Output} from '@angular/core';
 import {DokumentumService} from '../dokumentum.service';
 import {FajlBuf} from '../fajlbuf';
 import {ErrorService} from '../../tools/errorbox/error.service';
 import {DokumentumDto} from '../dokumentumdto';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-dokumentum-feltoltes',
   templateUrl: './dokumentum-feltoltes.component.html'
 })
 export class DokumentumFeltoltesComponent implements OnDestroy {
-  @ViewChild('fileInput', {static: true}) fileInput: ElementRef;
-
-  file: any;
-  file64: any;
-  fajlnev = '';
-  megjegyzes = '';
-  imageSrc: string;
-
   @Input() Iratkod = -1;
   @Output() eventSzerkeszteskesz = new EventEmitter<DokumentumDto>();
 
+  fb: any;
+
+  form: FormGroup;
   eppFrissit = false;
 
   dokumentumservice: DokumentumService;
 
   constructor(private _errorservice: ErrorService,
+              private _fb: FormBuilder,
               dokumentumservice: DokumentumService) {
     this.dokumentumservice = dokumentumservice;
+
+    this.form = this._fb.group({
+      'fajlnev': [{value: '', disabled: true}, []],
+      'megjegyzes': ['', []]
+    });
   }
 
   onFileChange(event) {
     const reader = new FileReader();
     if (event.target.files && event.target.files.length > 0) {
-      this.file = event.target.files[0];
-      this.fajlnev = this.file.name;
-      reader.readAsDataURL(this.file);
+      const file = event.target.files[0];
+
+      this.form.controls['fajlnev'].setValue(file.name);
+
+      reader.readAsDataURL(file);
       reader.onload = () => {
-        this.imageSrc = reader.result as string;
-        this.file64 = (reader.result as string).split(',')[1];
+        const file64 = (reader.result as string).split(',')[1];
+
+        this.fb = new FajlBuf();
+        this.fb.b = file64;
+        this.fb.Fajlnev = file.name;
+        this.fb.Meret = file.size;
+        this.fb.IratKod = this.Iratkod;
       };
     }
   }
 
   onSubmit() {
-    const fb = new FajlBuf();
-    fb.b = this.file64;
-    fb.Fajlnev = this.fajlnev;
-    fb.Meret = this.file.size;
-    fb.Megjegyzes = this.megjegyzes;
-    fb.IratKod = this.Iratkod;
-
     this.eppFrissit = true;
-    this.dokumentumservice.FeltoltesAngular(fb)
+    this.fb.Megjegyzes = this.form.value['megjegyzes'];
+
+    this.dokumentumservice.FeltoltesAngular(this.fb)
       .then(res => {
         if (res.Error != null) {
           throw res.Error;

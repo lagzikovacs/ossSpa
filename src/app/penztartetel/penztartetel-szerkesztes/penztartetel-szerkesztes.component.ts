1869eb0bf3ac7637f6df19b3231b1557d8a63955
lks <lks@gridsolar.hu>
2021-12-15 20:55:57 +0100

    pénztártétel

--- src/app/penztartetel/penztartetel-szerkesztes/penztartetel-szerkesztes.component.ts
+++ src/app/penztartetel/penztartetel-szerkesztes/penztartetel-szerkesztes.component.ts
@@ -3,32 +3,40 @@ import {PenztartetelService} from '../penztartetel.service';
 import * as moment from 'moment';
 import {ErrorService} from '../../tools/errorbox/error.service';
 import {PenztartetelDto} from '../penztarteteldto';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-penztartetel-szerkesztes',
   templateUrl: './penztartetel-szerkesztes.component.html'
 })
-export class PenztartetelSzerkesztesComponent implements AfterViewInit, OnInit, OnDestroy {
+export class PenztartetelSzerkesztesComponent implements OnInit, OnDestroy {
   @ViewChild('jogcim', {static: true}) jogcimInput: ElementRef;
-  @ViewChild('ugyfel', {static: true}) ugyfelInput: ElementRef;
-  @ViewChild('bizonylatszam', {static: true}) bizonylatszamInput: ElementRef;
-  @ViewChild('bevetel', {static: true}) bevetelInput: ElementRef;
-  @ViewChild('kiadas', {static: true}) kiadasInput: ElementRef;
 
   @Input() Penztarkod = -1;
 
   DtoEdited = new PenztartetelDto();
-  datum = moment().format('YYYY-MM-DD');
 
   @Output() eventSzerkeszteskesz = new EventEmitter<PenztartetelDto>();
 
+  form: FormGroup;
   eppFrissit = false;
 
   penztartetelservice: PenztartetelService;
 
   constructor(private _errorservice: ErrorService,
+              private _fb: FormBuilder,
               penztartetelservice: PenztartetelService) {
     this.penztartetelservice = penztartetelservice;
+
+    this.form = this._fb.group({
+      'datum': ['', [Validators.required]],
+      'jogcim': ['', [Validators.required, Validators.maxLength(50)]],
+      'bevetel': [0, []],
+      'kiadas': [0, []],
+      'ugyfel': ['', [Validators.maxLength(200)]],
+      'bizonylatszam': ['', [Validators.maxLength(20)]],
+      'megjegyzes': ['', [Validators.maxLength(200)]],
+    });
   }
 
   ngOnInit() {
@@ -40,6 +48,12 @@ export class PenztartetelSzerkesztesComponent implements AfterViewInit, OnInit, 
         }
 
         this.DtoEdited = res.Result[0];
+        this.DtoEdited.Penztarkod = this.Penztarkod;
+
+        this.updateform();
+        this.jogcimInput.nativeElement.selectedIndex = 2;
+        this.jogcimchange();
+
         this.eppFrissit = false;
       })
       .catch(err => {
@@ -48,56 +62,73 @@ export class PenztartetelSzerkesztesComponent implements AfterViewInit, OnInit, 
         });
   }
 
-  ngAfterViewInit() {
-    this.jogcimInput.nativeElement.selectedIndex = 2;
-    this.jogcimchange();
-  }
-
   jogcimchange() {
-    this.ugyfelInput.nativeElement.disabled = false;
-    this.bizonylatszamInput.nativeElement.disabled = false;
-    this.bevetelInput.nativeElement.disabled = false;
-    this.kiadasInput.nativeElement.disabled = false;
+    this.form.controls['ugyfel'].enable();
+    this.form.controls['bizonylatszam'].enable();
+    this.form.controls['bevetel'].enable();
+    this.form.controls['kiadas'].enable();
 
     switch (this.jogcimInput.nativeElement.selectedIndex) {
       case 0: // Bevét korrekció
-        this.ugyfelInput.nativeElement.disabled = true;
-        this.bizonylatszamInput.nativeElement.disabled = true;
-        this.bevetelInput.nativeElement.disabled = true;
+        this.form.controls['ugyfel'].disable();
+        this.form.controls['bizonylatszam'].disable();
+        this.form.controls['bevetel'].disable();
         break;
       case 1: // Kiadás korrekció
-        this.ugyfelInput.nativeElement.disabled = true;
-        this.bizonylatszamInput.nativeElement.disabled = true;
-        this.kiadasInput.nativeElement.disabled = true;
+        this.form.controls['ugyfel'].disable();
+        this.form.controls['bizonylatszam'].disable();
+        this.form.controls['kiadas'].disable();
         break;
       case 2: // pénzfelvét bankból
-        this.ugyfelInput.nativeElement.disabled = true;
-        this.bizonylatszamInput.nativeElement.disabled = true;
-        this.kiadasInput.nativeElement.disabled = true;
+        this.form.controls['ugyfel'].disable();
+        this.form.controls['bizonylatszam'].disable();
+        this.form.controls['kiadas'].disable();
         break;
       case 3: // befizetés bankba
-        this.ugyfelInput.nativeElement.disabled = true;
-        this.bizonylatszamInput.nativeElement.disabled = true;
-        this.bevetelInput.nativeElement.disabled = true;
+        this.form.controls['ugyfel'].disable();
+        this.form.controls['bizonylatszam'].disable();
+        this.form.controls['bevetel'].disable();
         break;
       case 4: // Bejövő számla
-        this.bevetelInput.nativeElement.disabled = true;
+        this.form.controls['bevetel'].disable();
         break;
       case 5: // Kimenő számla
-        this.kiadasInput.nativeElement.disabled = true;
+        this.form.controls['kiadas'].disable();
         break;
       case 6: // Bérkifizetés
-        this.bizonylatszamInput.nativeElement.disabled = true;
-        this.bevetelInput.nativeElement.disabled = true;
+        this.form.controls['bizonylatszam'].disable();
+        this.form.controls['bevetel'].disable();
         break;
     }
   }
 
-  onSubmit() {
-    this.DtoEdited.Penztarkod = this.Penztarkod;
-    this.DtoEdited.Datum = moment(this.datum).toISOString(true);
+  updateform() {
+    const formDatum = moment(this.DtoEdited.Datum).format('YYYY-MM-DD');
 
+    this.form.controls['datum'].setValue(formDatum);
+    this.form.controls['jogcim'].setValue(this.DtoEdited.Jogcim);
+    this.form.controls['bevetel'].setValue(this.DtoEdited.Bevetel);
+    this.form.controls['kiadas'].setValue(this.DtoEdited.Kiadas);
+    this.form.controls['ugyfel'].setValue(this.DtoEdited.Ugyfelnev);
+    this.form.controls['bizonylatszam'].setValue(this.DtoEdited.Bizonylatszam);
+    this.form.controls['megjegyzes'].setValue(this.DtoEdited.Megjegyzes);
+  }
+  updatedto() {
+    const dtoDatum = moment(this.form.value['datum']).toISOString(true);
+
+    this.DtoEdited.Datum = dtoDatum;
+    this.DtoEdited.Jogcim = this.form.value['jogcim'];
+    this.DtoEdited.Bevetel = this.form.value['bevetel'];
+    this.DtoEdited.Kiadas = this.form.value['kiadas'];
+    this.DtoEdited.Ugyfelnev = this.form.value['ugyfel'];
+    this.DtoEdited.Bizonylatszam = this.form.value['bizonylatszam'];
+    this.DtoEdited.Megjegyzes = this.form.value['megjegyzes'];
+  }
+
+  onSubmit() {
     this.eppFrissit = true;
+    this.updatedto();
+
     this.penztartetelservice.Add(this.DtoEdited)
       .then(res => {
         if (res.Error != null) {

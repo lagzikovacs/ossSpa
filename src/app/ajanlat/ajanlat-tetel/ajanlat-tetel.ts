6ebf584b70317d4569881c7794457e664b16479f
lks <lks@gridsolar.hu>
2022-01-23 10:42:58 +0100

    ajánlattétel

--- src/app/ajanlat/ajanlat-tetel/ajanlat-tetel.ts
+++ src/app/ajanlat/ajanlat-tetel/ajanlat-tetel.ts
@@ -2,6 +2,7 @@ import {ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit, Ou
 import {AjanlatSzerkesztesMode} from '../ajanlatszerkesztesmode';
 import {CikkDto} from '../../cikk/cikkdto';
 import {AjanlatBuf} from '../ajanlatbuf';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-ajanlat-tetel',
@@ -12,35 +13,61 @@ export class AjanlatTetelComponent implements OnInit, OnDestroy {
   @Output() eventTetelKesz = new EventEmitter<AjanlatBuf>();
 
   ajanlatzoombox: any;
-
   AjanlatSzerkesztesMode = AjanlatSzerkesztesMode.Blank;
+  formTetel: FormGroup;
 
-  constructor(private _cdr: ChangeDetectorRef) {
+  constructor(private _fb: FormBuilder) {
+    this.formTetel = this._fb.group({
+      'cikk': ['', [Validators.required, Validators.maxLength(100)]],
+      'mennyiseg': [0, [Validators.required, Validators.min(0)]],
+      'egysegar': [0, [Validators.required, Validators.min(0)]],
+      'egysegnyi': [0, []],
+      'garancia': [0, [Validators.required, Validators.min(0)]],
+    });
   }
 
   ngOnInit() {
     this.ajanlatzoombox = document.getElementById('ajanlatzoombox');
+
+    this.updateform();
+  }
+
+  updateform() {
+    this.formTetel.controls['cikk'].setValue(this.item.CikkNev);
+    this.formTetel.controls['mennyiseg'].setValue(this.item.Mennyiseg);
+    this.formTetel.controls['egysegar'].setValue(this.item.EgysegAr);
+    this.formTetel.controls['egysegnyi'].setValue(this.item.EgysegnyiTeljesitmeny);
+    this.formTetel.controls['garancia'].setValue(this.item.Garancia);
+  }
+  updatedto() {
+    this.item.CikkNev = this.formTetel.value['cikk'];
+    this.item.Mennyiseg = this.formTetel.value['mennyiseg'];
+    this.item.EgysegAr = this.formTetel.value['egysegar'];
+    this.item.EgysegnyiTeljesitmeny = this.formTetel.value['egysegnyi'];
+    this.item.Garancia = this.formTetel.value['garancia'];
   }
 
   CikkZoom() {
+    this.updatedto();
     this.AjanlatSzerkesztesMode = AjanlatSzerkesztesMode.CikkZoom;
     this.ajanlatzoombox.style.display = 'block';
-    this._cdr.detectChanges();
   }
   onCikkSelectzoom(Dto: CikkDto) {
     this.item.CikkKod = Dto.Cikkkod;
     this.item.CikkNev = Dto.Megnevezes;
     this.item.AfaMerteke = Dto.Afamerteke;
     this.item.EgysegAr = Dto.Egysegar;
-    this._cdr.detectChanges();
+
+    this.updateform();
   }
   onCikkStopzoom() {
     this.AjanlatSzerkesztesMode = AjanlatSzerkesztesMode.Blank;
     this.ajanlatzoombox.style.display = 'none';
-    this._cdr.detectChanges();
   }
 
   onSubmit() {
+    this.updatedto();
+    
     this.eventTetelKesz.emit(this.item);
   }
   cancel() {

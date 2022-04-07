fe378a74b99f0991617d7ebff91e97e327a07455
lks <lks@gridsolar.hu>
2022-04-07 07:35:03 +0200

    cikk szerkesztés

--- src/app/01 Torzsadatok/06 Cikk/cikk-szerkesztes/cikk-szerkesztes.component.ts
+++ src/app/01 Torzsadatok/06 Cikk/cikk-szerkesztes/cikk-szerkesztes.component.ts
@@ -1,26 +1,36 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {CikkService} from '../cikk.service';
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit,
+  Output, ViewChild, ViewContainerRef
+} from '@angular/core';
 import {MeService} from '../../04 Mennyisegiegyseg/me.service';
 import {AfakulcsService} from '../../05 Afakulcs/afakulcs.service';
-import {TermekdijService} from '../../051 Termekdij/termekdij.service';
-import {MeZoomParam} from '../../04 Mennyisegiegyseg/mezoomparam';
-import {EmptyResult} from '../../../common/dtos/emptyresult';
-import {TermekdijZoomParam} from '../../051 Termekdij/termekdijzoomparam';
-import {CikkSzerkesztesMode} from '../cikkszerkesztesmode';
 import {ErrorService} from '../../../common/errorbox/error.service';
 import {deepCopy} from '../../../common/deepCopy';
 import {AfakulcsDto} from '../../05 Afakulcs/afakulcsdto';
 import {MeDto} from '../../04 Mennyisegiegyseg/medto';
-import {TermekdijDto} from '../../051 Termekdij/termekdijdto';
 import {CikkDto} from '../cikkdto';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MeZoomParam} from '../../04 Mennyisegiegyseg/mezoomparam';
 import {AfakulcsZoomParam} from '../../05 Afakulcs/afakulcszoomparam';
+import {CikkService} from '../cikk.service';
+import {OnDestroyMixin, untilComponentDestroyed} from '@w11k/ngx-componentdestroyed';
+import {ModalService} from '../../../common/modal/modal.service';
+import {MeListComponent} from '../../04 Mennyisegiegyseg/me-list/me-list.component';
+import {AfakulcsListComponent} from '../../05 Afakulcs/afakulcs-list/afakulcs-list.component';
+import {TermekdijListComponent} from '../../051 Termekdij/termekdij-list/termekdij-list.component';
+import {NumberResult} from '../../../common/dtos/numberresult';
+import {TermekdijService} from '../../051 Termekdij/termekdij.service';
+import {TermekdijZoomParam} from '../../051 Termekdij/termekdijzoomparam';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-cikk-szerkesztes',
   templateUrl: './cikk-szerkesztes.component.html'
 })
-export class CikkSzerkesztesComponent implements OnInit, OnDestroy {
+export class CikkSzerkesztesComponent extends OnDestroyMixin implements OnInit, OnDestroy {
+  @ViewChild('compcont_cikkszerk', {read: ViewContainerRef}) vcr: ViewContainerRef;
+  modalname = 'modal_cikkszerk';
+
   @Input() uj = false;
   DtoEdited = new CikkDto();
   @Input() set DtoOriginal(value: CikkDto) {
@@ -30,10 +40,10 @@ export class CikkSzerkesztesComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
   eppFrissit = false;
-
-  cikkzoombox: any;
-
-  SzerkesztesMode = CikkSzerkesztesMode.Blank;
+  set spinner(value: boolean) {
+    this.eppFrissit = value;
+    this.docdr();
+  }
 
   cikkservice: CikkService;
 
@@ -41,8 +51,12 @@ export class CikkSzerkesztesComponent implements OnInit, OnDestroy {
               private _afakulcsservice: AfakulcsService,
               private _termekdijservice: TermekdijService,
               private _errorservice: ErrorService,
+              private _modalservice: ModalService,
               private _fb: FormBuilder,
+              private _cdr: ChangeDetectorRef,
               cikkservice: CikkService) {
+    super();
+
     this.cikkservice = cikkservice;
 
     this.form = this._fb.group({
@@ -58,29 +72,29 @@ export class CikkSzerkesztesComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnInit() {
-    this.cikkzoombox = document.getElementById('cikkzoombox');
-
+  async ngOnInit() {
     if (this.uj) {
-      this.eppFrissit = true;
-      this.cikkservice.CreateNew()
-        .then(res => {
-          if (res.Error !== null) {
-            throw res.Error;
-          }
+      this.spinner = true;
+      try {
+        const res = await this.cikkservice.CreateNew();
+        if (res.Error !== null) {
+          throw res.Error;
+        }
 
-          this.DtoEdited = res.Result[0];
-          this.updateform();
-          this.eppFrissit = false;
-        })
-        .catch(err => {
-          this.eppFrissit = false;
-          this._errorservice.Error = err;
-        });
-    } else {
-      this.updateform();
+        this.DtoEdited = res.Result[0];
+        this.spinner = false;
+      } catch (err) {
+        this.spinner = false;
+        this._errorservice.Error = err;
+      }
     }
 
+    this.updateform();
+  }
+
+  docdr() {
+    this._cdr.markForCheck();
+    this._cdr.detectChanges();
   }
 
   updateform() {
@@ -90,6 +104,7 @@ export class CikkSzerkesztesComponent implements OnInit, OnDestroy {
     this.form.controls['egysegar'].setValue(this.DtoEdited.Egysegar);
     this.form.controls['kk'].setValue(this.DtoEdited.Keszletetkepez);
     this.form.controls['tomeg'].setValue(this.DtoEdited.Tomegkg);
+
     this.form.controls['termekdijkt'].setValue(this.DtoEdited.Termekdijkt);
     this.form.controls['termekdijmegnevezes'].setValue(this.DtoEdited.Termekdijmegnevezes);
     this.form.controls['termekdijegysegar'].setValue(this.DtoEdited.Termekdijegysegar);
@@ -99,125 +114,125 @@ export class CikkSzerkesztesComponent implements OnInit, OnDestroy {
     this.DtoEdited.Me = this.form.value['me'];
     this.DtoEdited.Afakulcs = this.form.value['afakulcs'];
     this.DtoEdited.Egysegar = this.form.value['egysegar'];
-    this.DtoEdited.Keszletetkepez = this.form.value['kk'];
+    this.DtoEdited.Keszletetkepez = this.form.value['kk'] === 'true';
     this.DtoEdited.Tomegkg = this.form.value['tomeg'];
+
     this.DtoEdited.Termekdijkt = this.form.value['termekdijkt'];
-    this.DtoEdited.Termekdijmegnevezes = this.form.value['termekdijmegnevezes'];
-    this.DtoEdited.Termekdijegysegar = this.form.value['termekdijegysegar'];
   }
 
-  onSubmit() {
-    this.eppFrissit = true;
+  async onSubmit() {
     this.updatedto();
 
-    this._meservice.ZoomCheck(new MeZoomParam(this.DtoEdited.Mekod || 0,
-      this.DtoEdited.Me || ''))
-      .then(res => {
-        if (res.Error !== null) {
-          throw res.Error;
-        }
-        return this._afakulcsservice.ZoomCheck(new AfakulcsZoomParam(this.DtoEdited.Afakulcskod || 0,
-          this.DtoEdited.Afakulcs || ''));
-      })
-      .then(res1 => {
-        if (res1.Error !== null) {
-          throw res1.Error;
-        }
+    this.spinner = true;
+    try {
+      const res = await this._meservice.ZoomCheck(new MeZoomParam(this.DtoEdited.Mekod || 0,
+        this.DtoEdited.Me || ''));
+      if (res.Error !== null) {
+        throw res.Error;
+      }
 
-        if ((this.DtoEdited.Termekdijkt || '') !== '') {
-          return this._termekdijservice.ZoomCheck(new TermekdijZoomParam(this.DtoEdited.Termekdijkod || 0,
-            this.DtoEdited.Termekdijkt || ''));
-        } else {
-          this.DtoEdited.Termekdijkod = undefined;
-          this.DtoEdited.Termekdijkt = undefined;
-          this.DtoEdited.Termekdijmegnevezes = undefined;
-          this.DtoEdited.Termekdijegysegar = undefined;
-          return new Promise<EmptyResult>((resolve, reject) => { resolve(new EmptyResult()); });
-        }
-      })
-      .then(res2 => {
+      const res1 = await this._afakulcsservice.ZoomCheck(new AfakulcsZoomParam(this.DtoEdited.Afakulcskod || 0,
+        this.DtoEdited.Afakulcs || ''));
+      if (res1.Error !== null) {
+        throw res1.Error;
+      }
+
+      if (this.DtoEdited.Termekdijkt || '' !== '') {
+        const res2 = await this._termekdijservice.ZoomCheck(new TermekdijZoomParam(this.DtoEdited.Termekdijkod || 0,
+          this.DtoEdited.Termekdijkt || ''));
         if (res2.Error !== null) {
           throw res2.Error;
         }
+      }
 
-        // itt a zoom már le van ellenőrizve
-        if (this.uj) {
-          return this.cikkservice.Add(this.DtoEdited);
-        } else {
-          return this.cikkservice.Update(this.DtoEdited);
-        }
-      })
-      .then(res3 => {
-        if (res3.Error !== null) {
-          throw res3.Error;
-        }
-        return this.cikkservice.Get(res3.Result);
-      })
-      .then(res4 => {
-        if (res4.Error !== null) {
-          throw res4.Error;
-        }
+      let res3: NumberResult;
+      if (this.uj) {
+        res3 = await this.cikkservice.Add(this.DtoEdited);
+      } else {
+        res3 = await this.cikkservice.Update(this.DtoEdited);
+      }
+      if (res3.Error !== null) {
+        throw res3.Error;
+      }
 
-        this.eppFrissit = false;
-        this.eventSzerkeszteskesz.emit(res4.Result[0]);
-      })
-      .catch(err => {
-        this.eppFrissit = false;
-        this._errorservice.Error = err;
-      });
+      const res4 = await this.cikkservice.Get(res3.Result);
+      if (res4.Error !== null) {
+        throw res4.Error;
+      }
+
+      this.spinner = false;
+      this.eventSzerkeszteskesz.emit(res4.Result[0]);
+    } catch (err) {
+      this.spinner = false;
+      this._errorservice.Error = err;
+    }
   }
 
   onCancel() {
-    this.eventSzerkeszteskesz.emit(null);
+    this.eventSzerkeszteskesz.emit();
   }
+
 
   MeZoom() {
     this.updatedto();
-    this.SzerkesztesMode = CikkSzerkesztesMode.MeZoom;
-    this.cikkzoombox.style.display = 'block';
-  }
-  onMeSelectzoom(Dto: MeDto) {
-    this.DtoEdited.Mekod = Dto.Mekod;
-    this.DtoEdited.Me = Dto.Me;
-    this.updateform();
-  }
-  onMeStopzoom() {
-    this.SzerkesztesMode = CikkSzerkesztesMode.Blank;
-    this.cikkzoombox.style.display = 'none';
+
+    this.vcr.clear();
+    const meC = this.vcr.createComponent(MeListComponent);
+    meC.instance.maszk = this.DtoEdited.Me || '';
+    meC.instance.eventSelectzoom.pipe(untilComponentDestroyed(this)).subscribe(dto => {
+      this.DtoEdited.Mekod = dto.Mekod;
+      this.DtoEdited.Me = dto.Me;
+      this.updateform();
+    });
+    meC.instance.eventStopzoom.pipe(untilComponentDestroyed(this)).subscribe(() => {
+      this.vcr.clear();
+      this._modalservice.close(this.modalname);
+    });
+
+    this._modalservice.open(this.modalname);
   }
 
   AfakulcsZoom() {
     this.updatedto();
-    this.SzerkesztesMode = CikkSzerkesztesMode.AfakulcsZoom;
-    this.cikkzoombox.style.display = 'block';
-  }
-  onAfakulcsSelectzoom(Dto: AfakulcsDto) {
-    this.DtoEdited.Afakulcskod = Dto.Afakulcskod;
-    this.DtoEdited.Afakulcs = Dto.Afakulcs1;
-    this.DtoEdited.Afamerteke = Dto.Afamerteke;
-    this.updateform();
-  }
-  onAfakulcsStopzoom() {
-    this.SzerkesztesMode = CikkSzerkesztesMode.Blank;
-    this.cikkzoombox.style.display = 'none';
+
+    this.vcr.clear();
+    const afakulcsC = this.vcr.createComponent(AfakulcsListComponent);
+    afakulcsC.instance.maszk = this.DtoEdited.Afakulcs || '';
+    afakulcsC.instance.eventSelectzoom.pipe(untilComponentDestroyed(this)).subscribe(dto => {
+      this.DtoEdited.Afakulcskod = dto.Afakulcskod;
+      this.DtoEdited.Afakulcs = dto.Afakulcs1;
+      this.DtoEdited.Afamerteke = dto.Afamerteke;
+      this.updateform();
+    });
+    afakulcsC.instance.eventStopzoom.pipe(untilComponentDestroyed(this)).subscribe(() => {
+      this.vcr.clear();
+      this._modalservice.close(this.modalname);
+    });
+
+    this._modalservice.open(this.modalname);
   }
 
   TermekdijZoom() {
     this.updatedto();
-    this.SzerkesztesMode = CikkSzerkesztesMode.TermekdijZoom;
-    this.cikkzoombox.style.display = 'block';
+
+    this.vcr.clear();
+    const tdC = this.vcr.createComponent(TermekdijListComponent);
+    tdC.instance.maszk = this.DtoEdited.Termekdijkt || '';
+    tdC.instance.eventSelectzoom.pipe(untilComponentDestroyed(this)).subscribe(dto => {
+      this.DtoEdited.Termekdijkod = dto.Termekdijkod;
+      this.DtoEdited.Termekdijkt = dto.Termekdijkt;
+      this.DtoEdited.Termekdijmegnevezes = dto.Termekdijmegnevezes;
+      this.DtoEdited.Termekdijegysegar = dto.Termekdijegysegar;
+      this.updateform();
+    });
+    tdC.instance.eventStopzoom.pipe(untilComponentDestroyed(this)).subscribe(() => {
+      this.vcr.clear();
+      this._modalservice.close(this.modalname);
+    });
+
+    this._modalservice.open(this.modalname);
   }
-  onTermekdijSelectzoom(Dto: TermekdijDto) {
-    this.DtoEdited.Termekdijkod = Dto.Termekdijkod;
-    this.DtoEdited.Termekdijkt = Dto.Termekdijkt;
-    this.DtoEdited.Termekdijmegnevezes = Dto.Termekdijmegnevezes;
-    this.DtoEdited.Termekdijegysegar = Dto.Termekdijegysegar;
-    this.updateform();
-  }
-  onTermekdijStopzoom() {
-    this.SzerkesztesMode = CikkSzerkesztesMode.Blank;
-    this.cikkzoombox.style.display = 'none';
-  }
+
   TermekdijTorles() {
     this.updatedto();
     this.DtoEdited.Termekdijkod = null;
@@ -227,7 +242,9 @@ export class CikkSzerkesztesComponent implements OnInit, OnDestroy {
     this.updateform();
   }
 
-  ngOnDestroy() {
+  override ngOnDestroy(): void {
+    super.ngOnDestroy();
+
     Object.keys(this).map(k => {
       (this[k]) = null;
     });

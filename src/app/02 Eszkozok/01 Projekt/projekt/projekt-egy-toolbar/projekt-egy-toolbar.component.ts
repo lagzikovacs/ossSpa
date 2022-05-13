106dc5e4c4796a33096da5bb49e77294476a6ff1
lks <lks@gridsolar.hu>
2022-05-13 18:27:27 +0200

    pk majdnem+

---
+++ src/app/02 Eszkozok/01 Projekt/projekt/projekt-egy-toolbar/projekt-egy-toolbar.component.ts
@@ -0,0 +1,27 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {LogonService} from "../../../../05 Segedeszkozok/05 Bejelentkezes/logon.service";
+import {JogKod} from "../../../../common/enums/jogkod";
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-projekt-egy-toolbar',
+  templateUrl: './projekt-egy-toolbar.component.html'
+})
+export class ProjektEgyToolbarComponent implements OnDestroy {
+  @Output() eventNav: EventEmitter<number> = new EventEmitter<number>();
+  jog: boolean = false;
+
+  constructor(private _logonservice: LogonService) {
+    this.jog = _logonservice.Jogaim.includes(JogKod[JogKod.PROJEKTMOD]);
+  }
+
+  doNav(i: number) {
+    this.eventNav.emit(i);
+  }
+
+  ngOnDestroy(): void {
+    Object.keys(this).map(k => {
+      (this[k]) = null;
+    });
+  }
+}
